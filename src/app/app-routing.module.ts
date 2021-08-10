a9d1469480ff806a0e7f8ff80c798772782f4ca6
unknown <jhanvisdesai@gmail.com>
2021-08-10 20:37:44 +0530

    alarm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlarmComponent } from './alarm/alarm.component';
 import { ClockComponent } from './clock/clock.component';
 import { DigitalClockComponent } from './digital-clock/digital-clock.component';
 import { TimerComponent } from './timer/timer.component';
@@ -15,7 +16,11 @@ const routes: Routes = [
     component: TimerComponent,
     // pathMatch:'full'
   },
-  
+  {
+    path:"alarm",
+    component: AlarmComponent,
+    // pathMatch:'full'
+  },  
   {
     path:"",
     component: ClockComponent,
